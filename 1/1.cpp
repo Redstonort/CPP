26d6596965b3769982ce5bffc08511fafa107d91
Redstonort <148367022+Redstonort@users.noreply.github.com>
2023-10-18 19:18:56 +0300

    Lesson1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+
+int m = 0;
+int a = 0;
+int F = 0;
+int main()
+{
+    cout << "введите пожалуйста массу: ";
+    cin >> m;
+    cout << "введите пожалуйста ускорение: ";
+    cin >> a;
+    F = a * m;
+    cout << "Полученая сила: " << F << endl;
+    return 0;
+}
